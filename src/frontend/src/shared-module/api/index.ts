33b5a5255c91fdfca592c59956ba38016c7c1d96
Alexey Andriyanenko <alexlikescoockies@gmail.com>
2026-01-19 18:34:07 +0200

    chore: deploy frontend

--- src/frontend/src/shared-module/api/index.ts
+++ src/frontend/src/shared-module/api/index.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "src/shared-module/api/http-client/http-client";
 import { authStore } from "src/auth-module/store/auth.store.ts";
 
 export const appHttpClient = new HttpClient({
-  baseUrl: "http://localhost:5130/api/v1",
+  baseUrl: import.meta.env.VITE_APP_TITLE,
   interceptors: [
     async (request: XMLHttpRequest) => {
       if (request.status === 401 && window.location.pathname !== "/auth/login") {
